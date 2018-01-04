62963f340a1f09e15cea242be511df2104e5a5f4
Joshua P Panter <joshu@unfettered.net>
2018-01-04 13:28:21 -0500

    version bump

--- iqrcodes/plugin.php
+++ iqrcodes/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: IQRCodes
 Plugin URI: https://github.com/joshp23/YOURLS-IQRCodes
 Description: Integrated QR Codes
-Version: 1.4.6
+Version: 1.4.7
 Author: Josh Panter
 Author URI: https://unfettered.net
 */
